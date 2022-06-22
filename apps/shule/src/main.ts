b83b68635a27f01c70c25d717325e8a73490d4b8
Douglas Mogoba Nyamwaro <mogobanyamwaro@gmail.com>
2022-06-22 23:10:23 +0300

    chore: added swagger

--- apps/shule/src/main.ts
+++ apps/shule/src/main.ts
@@ -1,21 +1,58 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-
+import { ClassSerializerInterceptor, Logger } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { WinstonModule, utilities as WinstonUtilities } from 'nest-winston';
+import * as express from 'express';
+import * as winston from 'winston';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app/app.module';
+import * as cookieParser from 'cookie-parser';
+import * as compression from 'compression';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const server = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server), {
+    cors: true,
+    logger: WinstonModule.createLogger({
+      transports: [
+        new winston.transports.Console({
+          level: 'debug',
+          format: WinstonUtilities.format.nestLike('GoShule', {
+            prettyPrint: true,
+          }),
+        }),
+      ],
+    }),
+  });
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
+  app.enableShutdownHooks();
+  app.enableCors();
+  app.use(cookieParser());
+  app.use(compression());
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  const config = new DocumentBuilder()
+    .setTitle('Go Shule API')
+    .setDescription('The Go Shule API description')
+    .setVersion('1.0')
+    .addTag('Go Shule')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  const port = process.env.PORT || 5000;
   await app.listen(port);
+
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
+  Logger.log(
+    `🚀 Swagger is running on: http://localhost:${port}/${globalPrefix}/docs`
   );
 }
 
